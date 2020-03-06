697094d7703b4326a026d80f87beac4449326bab
maple <183517940@qq.com>
2020-03-06 19:24:54 +0800

    add dist folder

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
     chainWebpack: (config)=>{
         config.resolve.alias.set('@', resolve('src'));
     },
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     pages: {
         index: {
             entry: 'examples/main.js',
